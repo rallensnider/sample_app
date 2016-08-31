1f015e80e42ed8e0a69d5a25cff91c8ef7db508c
R. Allen Snider <rallensnider@gmail.com>
2016-08-31 13:07:48 -0700

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -63,17 +64,23 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
-  # Converts email to all lower-case.
-  def downcase_email
-    email.downcase!
-  end
+   # Converts email to all lower-case.
+    def downcase_email
+      email.downcase!
+    end
 
-  # Create and assigns the activation token and digest.
-  def create_activation_digest
-    self.activation_token = User.new_token
-    self.activation_digest = User.digest(activation_token)
-  end
+    # Create and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 
 end
